5376a4346a11f5447a551b22ea621f34a2304dc6
DJ Drakos <dianajodrakos@gmail.com>
2021-05-05 16:40:03 -0700

    constructed working getRandomThrow function

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import { countAsAYes, countAsANo, countSpacewalk } from './utils.js';
+import { getRandomThrow } from './get-random-throw.js';
 
 // import functions and grab DOM elements
 const inputButton = document.querySelector('#user-throw');
@@ -10,11 +10,13 @@ let rounds = 0;
 let wins = 0;
 
 // set event listeners to update state and DOM
-
 inputButton.addEventListener ('click', () => {
+    //grab user + CPU throws and store them in variables
     const input = document.querySelector('input[type=radio]:checked');
     let userThrow = input.value;
-    console.log('this button is working!');
+    let computerThrow = getRandomThrow();
+
+    console.log(computerThrow);
     console.log('input.value = ' + input.value + ', userThrow = ' + userThrow);
 
 });
